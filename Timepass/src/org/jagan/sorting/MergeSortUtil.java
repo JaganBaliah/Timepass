16c25bf3bfa596242ba08773342a0f57dbfdc494
Jagan Baliah <jagan.baliah@aa.com>
2020-04-28 18:52:46 -0500

    Adding more items to datapoints used for sorting

--- Timepass/src/org/jagan/sorting/MergeSortUtil.java
+++ Timepass/src/org/jagan/sorting/MergeSortUtil.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MergeSortUtil {
 
 	public static void main(String[] args) {
-		int[] arr = {12, 11, 13, 5, 6, 7};
+		int[] arr = {12, 11, 12, 13, 7, 5, 6, 7};
 		System.out.println(Arrays.toString(arr));
 		sort(arr, 0, arr.length - 1);
 		System.out.println(Arrays.toString(arr));
--- Timepass/src/org/jagan/sorting/SelectionSortUtil.java
+++ Timepass/src/org/jagan/sorting/SelectionSortUtil.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SelectionSortUtil {
 
 	public static void main(String[] args) {
-		int[] arr = {12, 11, 13, 5, 6, 7};
+		int[] arr = {12, 11, 12, 13, 7, 5, 6, 7};
 		System.out.println(Arrays.toString(arr));
 		sort(arr);
 		System.out.println(Arrays.toString(arr));
